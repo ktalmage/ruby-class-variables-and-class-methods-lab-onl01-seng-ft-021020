0b73a0f65eca3bb53a33ee2cd9460c8be7ca7efb
Kyle Talmage <ktalmage85@gmail.com>
2020-02-20 18:38:02 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -38,15 +38,10 @@ class Song
   
   def self.genre_count
     
-  @@genre_hash = Hash[@@genres.each do |genre,count| 
-    if @@genre.uniq,
-      return @@genre.count
-    else
-    end
-  end
+  
    
-   #@@genre_hash = Hash[@@genres.collect {|genre,count| [@@genres,@@count]}]
- #end
+   @@genre_hash = Hash[@@genres.collect {|genre,count| [@@genres,@@count]}]
+ end
   end
   
 #   def self.artist_count
