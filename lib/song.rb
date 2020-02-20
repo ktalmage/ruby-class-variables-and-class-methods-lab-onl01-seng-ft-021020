223031ff1005ae3a177aae69b6075a2670cc9a33
Kyle Talmage <ktalmage85@gmail.com>
2020-02-20 18:20:15 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -38,7 +38,7 @@ class Song
   
   def self.genre_count
     
-   @@genre_hash = Hash[@@genres.collect {|genre,count| [@@genres,@count]}]
+   @@genre_hash = Hash[@@genres.collect {|genre,count| [@@genres,@@count]}]
    end 
   end
   
